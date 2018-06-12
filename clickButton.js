c4cd26516675aeb8cbedcaf114809f5f9d05a66b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2018-06-12 03:25:31 -0700

    Update clickButton.js

--- clickButton.js
+++ clickButton.js
@@ -20,3 +20,33 @@ result.push(transformed);
 }
 
 document.getElementById("main_button").onClick = getText();
+
+
+
+
+// functional
+(function () {
+"use strict";
+var SomeText = function (text) {
+this.text=text;
+};
+SomeText.prototype.capify = function(str){
+var firstLetter = str.charAt(0);
+var remainer = str.substring(1);
+return [firstLetter.toUpperCase(), remainder].join("");
+};
+SomeText.prototype.capifyWords = function (){
+var result = [];
+var textArray = this.text.split(" ");
+for (var counter = 0; counter < textArray.length; counter++) {
+result.push(this.capify(textArray[counter]));
+}
+return result.join(" ");
+}
+
+document.getElementById("main_button").addEventListening("click", function(e){
+var something = prompt("Enter your string");
+var newText = new SomeText(something);
+alert(newText.capifyWorlds());
+})
+}());
