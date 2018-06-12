73cb8e88ed08a22bdb0caefb49e2450859c4eee5
Captain Storm <CaptainStorm21@users.noreply.github.com>
2018-06-12 02:17:04 -0700

    Create clickButton.js

---
+++ clickButton.js
@@ -0,0 +1,22 @@
+var result;
+function getText(){
+   var someText = prompt ("cap me");
+   capWords(someText);
+alert(result.join(""));
+}
+
+function capWords(input) {
+var counter;
+var inputArray = input.split(" ");
+var transformed = "";
+resut = [];
+for (counter = 0; counter < inputArray.length; counter++){
+	transformed = [
+inputArray[counter].charAt(0).toUpperCase(),
+inputArray[counter].substring(1)
+].join("");
+result.push(transformed);
+}
+}
+
+document.getElementById("main_button").onClick = getText();
